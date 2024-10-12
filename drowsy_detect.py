031dd74bdb0a6ce84b1f25303e6973e1511cf7c4
Dybios <dibyajyoti.samal@gmail.com>
2024-10-12 16:58:24 +0100

    Add initial code, model and HTML templates

---
+++ drowsy_detect.py
@@ -0,0 +1,214 @@
+# import the necessary packages
+from scipy.spatial import distance as dist # to compute euclidiean distance
+from imutils.video import VideoStream
+from flask import Response
+from flask import Flask
+from flask import render_template
+from imutils import face_utils
+import threading
+import argparse
+import datetime
+import imutils
+import time
+import cv2
+import dlib
+
+# defining eye aspect ratio according to research paper
+def eye_aspect_ratio(eye):
+	# compute the euclidean distances between the two sets of
+	# vertical eye landmarks (x, y)-coordinates
+	A = dist.euclidean(eye[1], eye[5])
+	B = dist.euclidean(eye[2], eye[4])
+	# compute the euclidean distance between the horizontal
+	# eye landmark (x, y)-coordinates
+	C = dist.euclidean(eye[0], eye[3])
+	# compute the eye aspect ratio
+	return (A + B) / (2.0 * C)
+
+# define two constants, one for the eye aspect ratio to indicate
+# blink and then a second constant for the number of consecutive
+# frames the eye must be below the threshold for to set off the
+# alarm
+EYE_AR_THRESH = 0.3
+EYE_AR_CONSEC_FRAMES = 48
+
+# initialize the output frame and a lock used to ensure thread-safe
+# exchanges of the output frames (useful when multiple browsers/tabs
+# are viewing the stream)
+outputFrame = None
+lock = threading.Lock()
+# initialize a flask object
+app = Flask(__name__)
+# initialize the video stream and allow the camera sensor to
+# warmup
+#vs = VideoStream(usePiCamera=1).start()
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
+
+@app.route("/")
+def index():
+	# return the rendered template
+	return render_template("index.html")
+
+def detect_motion(frameCount):
+	# grab global references to the video stream, output frame, and
+	# lock variables
+	global vs, outputFrame, lock
+	width = 400;
+
+	# initialize the total number of frames
+	# read thus far
+	total = 0
+
+	# initialize the frame counter as well as a boolean used to
+	# indicate if the alarm is going off
+	drowsy_counter = 0
+	alarm_flag = False
+
+	# loop over frames from the video stream
+	while True:
+		# read the next frame from the video stream, resize it,
+		# convert the frame to grayscale, and blur it
+		frame = vs.read()
+		frame = imutils.resize(frame, width)
+		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		rects = detector(gray, 0)
+		# grab the current timestamp and draw it on the frame
+		timestamp = datetime.datetime.now()
+		cv2.putText(frame, timestamp.strftime(
+			"%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+
+
+		# if the total number of frames has reached a sufficient
+		# number to construct a reasonable background model, then
+		# continue to process the frame
+		if total > frameCount:
+			# loop over the face detections
+			for rect in rects:
+				# determine the facial landmarks for the face region, then
+				# convert the facial landmark (x, y)-coordinates to a NumPy
+				# array
+				shape = predictor(gray, rect)
+				shape = face_utils.shape_to_np(shape)
+				# extract the left and right eye coordinates, then use the
+				# coordinates to compute the eye aspect ratio for both eyes
+				leftEye = shape[lStart:lEnd]
+				rightEye = shape[rStart:rEnd]
+				leftEAR = eye_aspect_ratio(leftEye)
+				rightEAR = eye_aspect_ratio(rightEye)
+				# average the eye aspect ratio together for both eyes
+				ear = (leftEAR + rightEAR) / 2.0
+
+				# compute the convex hull for the left and right eye, then
+				# visualize each of the eyes
+				leftEyeHull = cv2.convexHull(leftEye)
+				rightEyeHull = cv2.convexHull(rightEye)
+				cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+				cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+
+				# check to see if the eye aspect ratio is below the blink
+				# threshold, and if so, increment the blink frame counter
+				if ear < EYE_AR_THRESH:
+					drowsy_counter += 1
+					# if the eyes were closed for a sufficient number of
+					# then sound the alarm
+					if drowsy_counter >= EYE_AR_CONSEC_FRAMES:
+						# if the alarm is not on, turn it on
+						if not alarm_flag:
+							alarm_flag = True
+							# check to see if an alarm file was supplied,
+							# and if so, start a thread to have the alarm
+							# sound played in the background
+#							if args["alarm"] != "":
+#								t = Thread(target=sound_alarm,
+#									args=(args["alarm"],))
+#								t.deamon = True
+#								t.start()
+						# draw an alarm on the frame
+						cv2.putText(frame, "DROWSINESS ALERT!", (10, 30),
+							cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+						print("DROWSY!!")
+				# otherwise, the eye aspect ratio is not below the blink
+				# threshold, so reset the counter and alarm
+				else:
+					drowsy_counter = 0
+					alarm_flag = False
+
+				# draw the computed eye aspect ratio on the frame to help
+				# with debugging and setting the correct eye aspect ratio
+				# thresholds and frame counters
+				cv2.putText(frame, "EAR: {:.2f}".format(ear), ((width-30), 30),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 0, 255), 2)
+
+		total += 1
+		# acquire the lock, set the output frame, and release the
+		# lock
+		with lock:
+			outputFrame = frame.copy()
+
+def generate():
+	# grab global references to the output frame and lock variables
+	global outputFrame, lock
+	# loop over frames from the output stream
+	while True:
+		# wait until the lock is acquired
+		with lock:
+			# check if the output frame is available, otherwise skip
+			# the iteration of the loop
+			if outputFrame is None:
+				continue
+			# encode the frame in JPEG format
+			(flag, encodedImage) = cv2.imencode(".jpg", outputFrame)
+			# ensure the frame was successfully encoded
+			if not flag:
+				continue
+		# yield the output frame in the byte format
+		yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + 
+			bytearray(encodedImage) + b'\r\n')
+
+@app.route("/video_feed")
+def video_feed():
+	# return the response generated along with the specific media
+	# type (mime type)
+	return Response(generate(),
+		mimetype = "multipart/x-mixed-replace; boundary=frame")
+
+# check to see if this is the main thread of execution
+if __name__ == '__main__':
+	# construct the argument parser and parse command line arguments
+	ap = argparse.ArgumentParser()
+	ap.add_argument("-i", "--ip", type=str, required=True,
+		help="ip address of the device")
+	ap.add_argument("-o", "--port", type=int, required=True,
+		help="ephemeral port number of the server (1024 to 65535)")
+	ap.add_argument("-f", "--frame-count", type=int, default=32,
+		help="# of frames used to construct the background model")
+	ap.add_argument("-p", "--shape-predictor", required=True,
+		help="path to facial landmark predictor")
+	ap.add_argument("-w", "--webcam", type=int, default=0,
+		help="index of webcam on system") # integer controls the index of your built-in webcam/USB camera
+
+	args = vars(ap.parse_args())
+
+	# initialize dlib's face detector (HOG-based) and then create
+	# the facial landmark predictor
+	print("loading facial landmark predictor...")
+	detector = dlib.get_frontal_face_detector()
+	predictor = dlib.shape_predictor(args["shape_predictor"])
+
+	# grab the indexes of the facial landmarks for the left and
+	# right eye, respectively
+	(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+	(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+	# start a thread that will perform motion detection
+	t = threading.Thread(target=detect_motion, args=(
+		args["frame_count"],))
+	t.daemon = True
+	t.start()
+	# start the flask app
+	app.run(host=args["ip"], port=args["port"], debug=True,
+		threaded=True, use_reloader=False)
+# release the video stream pointer
+vs.stop()
